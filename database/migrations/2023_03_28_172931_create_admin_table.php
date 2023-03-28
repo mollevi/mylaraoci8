fba32dc179950f35ea309a0ec8ac0a243c1be533
Kaako23 <csonkaakos23@gmail.com>
2023-03-28 20:25:54 +0200

    Migration-ök leírása

---
+++ database/migrations/2023_03_28_172931_create_admin_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("nev");
+            $table->string("email");
+            $table->timestamp("szuldatum");
+            $table->string("jelszo");
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+};
---
+++ database/migrations/2023_03_28_173700_create_megallo_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('megallo', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("nev");
+            $table->integer("kilometer");
+            $table->integer("vonat_id");
+            $table->integer("tavolsagi_buszok_id");
+            $table->integer("helyi_buszok_id");
+            $table->date("indulasi_ido");
+            $table->date("erkezesi_ido");
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('megallo');
+    }
+};
