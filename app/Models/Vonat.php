4e004aba9f095982e663c66c095fbbb296847f5a
mollevi <mollevi99@gmail.com>
2023-04-24 15:01:24 +0200

    draft: megallok kiiratasa select kattintás után v3.0

--- app/Models/Vonat.php
+++ app/Models/Vonat.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Vonatok extends Model
+class Vonat extends Model
 {
     use HasFactory;
 
