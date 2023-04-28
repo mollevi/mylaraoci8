c45ea36dcd82e3d4c9df5f8508ef3538d9e3a5dc
mollevi <mollevi99@gmail.com>
2023-04-28 10:24:49 +0200

    guest middleware miatt a bejelentkezett felhasznalo lepjen at loginrol a home-ra

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,12 +19,11 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
-
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
+        if (Auth::check()) {
+            return redirect("/home");
+        }
+        if (Auth::guard("admin")->check()) {
+            return redirect("/admin/home");
         }
 
         return $next($request);
